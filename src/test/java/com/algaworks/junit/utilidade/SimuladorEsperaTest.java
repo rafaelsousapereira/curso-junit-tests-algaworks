e0d8d26b69aae7b1778ffadb99ac1f27aae4a8d3
Rafael Sousa Pereira <rafael.sousa.pereira@mail.usf.edu.br>
2025-01-11 15:58:50 -0300

    feat: ignore unit test with @Disabled annotation

--- src/test/java/com/algaworks/junit/utilidade/SimuladorEsperaTest.java
+++ src/test/java/com/algaworks/junit/utilidade/SimuladorEsperaTest.java
@@ -1,6 +1,7 @@
 package com.algaworks.junit.utilidade;
 
 import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.Test;
 
 import java.time.Duration;
@@ -10,6 +11,7 @@ import static org.junit.jupiter.api.Assertions.*;
 class SimuladorEsperaTest {
 
     @Test
+    @Disabled("Não é mais aplicável")
     void deveEsperarENaoDarTimeout() {
         Assertions.assertTimeoutPreemptively(Duration.ofSeconds(1), () -> SimuladorEspera.esperar(Duration.ofMillis(10)));
     }
