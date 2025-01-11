23864467e2aaac7aaf558b5eda154388b21afb31
Rafael Sousa Pereira <rafael.sousa.pereira@mail.usf.edu.br>
2025-01-11 14:56:59 -0300

    feat: add assertions to exceptions throws

--- src/test/java/com/algaworks/junit/utilidade/SaudacaoUtilTest.java
+++ src/test/java/com/algaworks/junit/utilidade/SaudacaoUtilTest.java
@@ -2,7 +2,7 @@ package com.algaworks.junit.utilidade;
 
 import org.junit.jupiter.api.Test;
 
-import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.*;
 
 class SaudacaoUtilTest {
 
@@ -10,5 +10,17 @@ class SaudacaoUtilTest {
     public void testSaldar() {
         String saudacao = SaudacaoUtil.saudar(9);
         assertEquals("Bom dia", saudacao, "Saudação incorreta!");
+    }
+
+    @Test
+    public void deveLancarException() {
+        IllegalArgumentException illegalArgumentException = assertThrows(IllegalArgumentException.class,
+                () -> SaudacaoUtil.saudar(-10));
+        assertEquals("Hora inválida", illegalArgumentException.getMessage());
+    }
+
+    @Test
+    public void naoDeveLancarException() {
+        assertDoesNotThrow(() -> SaudacaoUtil.saudar(10));
     }
 }
