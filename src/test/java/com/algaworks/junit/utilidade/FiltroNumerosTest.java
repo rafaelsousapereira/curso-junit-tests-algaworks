72763f5545548a85dc46dad26e51b445922e2eb4
Rafael Sousa Pereira <rafael.sousa.pereira@mail.usf.edu.br>
2025-01-12 13:47:06 -0300

    test: change display name from tests with @DisplayNameGenaration annotation

--- src/test/java/com/algaworks/junit/utilidade/FiltroNumerosTest.java
+++ src/test/java/com/algaworks/junit/utilidade/FiltroNumerosTest.java
@@ -1,15 +1,18 @@
 package com.algaworks.junit.utilidade;
 
 import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.DisplayNameGeneration;
+import org.junit.jupiter.api.DisplayNameGenerator;
 import org.junit.jupiter.api.Test;
 
 import java.util.Arrays;
 import java.util.List;
 
+@DisplayNameGeneration(DisplayNameGenerator.ReplaceUnderscores.class)
 class FiltroNumerosTest {
 
     @Test
-    public void deveRetornarNumerosPares() {
+    public void deve_retornar_numeros_pares_() {
         List<Integer> numeros = Arrays.asList(1, 2, 3, 4);
         List<Integer> numerosParesEsperados = Arrays.asList(2, 4);
         List<Integer> numerosPares = FiltroNumeros.numerosPares(numeros);
