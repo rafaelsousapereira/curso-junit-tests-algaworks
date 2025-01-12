b6f9cd8bbf0e218b679280f38b57a03d4d0b4b6e
Rafael Sousa Pereira <rafael.sousa.pereira@mail.usf.edu.br>
2025-01-12 12:50:00 -0300

    test: immplement methods from ContaBancaria class and write junit unit test two same

--- src/test/java/com/algaworks/junit/utilidade/ContaBancariaTest.java
+++ src/test/java/com/algaworks/junit/utilidade/ContaBancariaTest.java
@@ -5,7 +5,7 @@ import org.junit.jupiter.api.Test;
 
 import java.math.BigDecimal;
 
-class ContaBancariaTest {
+public  class ContaBancariaTest {
 
     @Test
     void deveLancarExceptionQuandoSaldoForNulo() {
